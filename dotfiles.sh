7b7a9e4f690d4ea5a7c3d323f897d223e38c6c10
sanjay karinje <sanjay.karinje.8@gmail.com>
2024-02-08 14:17:46 -0800

    hardcoded dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,8 +2,7 @@ cd
 shopt -s expand_aliases
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 echo ".cfg" >> .gitignore
-read -e -p "Please paste a HTTPS link to your dotfiles repository (or press enter to use defaults): " DOTFILES_URL
-[[ -z $DOTFILES_URL ]] && DOTFILES_URL=https://github.com/sanjaykarinje/dotfiles.git
+DOTFILES_URL=https://github.com/sanjaykarinje/dotfiles.git
 git clone --bare $DOTFILES_URL .cfg/
 config checkout
 # only continue if checkout had no errors
@@ -28,10 +27,10 @@ if [[ $? = 0 ]]; then
   fi
   . ~/.bashrc
 
-  [[ -z $NAME  ]] && read -e -p "Enter your name (for git configuration): " NAME
-  [[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL
-  [[ $NAME  ]] && git config --global user.name "$NAME"
-  [[ $EMAIL ]] && git config --global user.email "$EMAIL"
+  NAME="sanjay karinje"
+  EMAIL="sanjay.karinje.8@gmail.com"
+  git config --global user.name "$NAME"
+  git config --global user.email "$EMAIL"
   git config --global credential.helper cache
   git config --global credential.helper 'cache --timeout=3600'
 
